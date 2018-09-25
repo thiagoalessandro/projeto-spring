dfc963dc572ce6aed9a9534f135a5de525325fbf
thiago.alessandro.farias <thiago.alessandro.farias@gmail.com>
2018-09-24 23:40:30 -0300

    ok 1

--- src/main/java/br/com/thiago/Projeto1Application.java
+++ src/main/java/br/com/thiago/Projeto1Application.java
@@ -3,7 +3,7 @@ package br.com.thiago;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class Projeto1Application {
 
 	public static void main(String[] args) {
